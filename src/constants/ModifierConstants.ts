dbd9ea9fc2f1bea618c87c005e09274fec4c498a
KumaV1 <KumaV1>
2024-01-30 18:45:40 +0100

    Monster type icon resources for base-defined types now uses paths, to better support offline play

--- src/constants/ModifierConstants.ts
+++ src/constants/ModifierConstants.ts
@@ -16,12 +16,13 @@ export class ModifierConstants {
     // Path instead of URL as the image is specific to this mod and can therefore be retrieced by "modContext.getResourceUrl"
     static readonly GENERIC_MODIFIER_ICON_RESOURCE_PATH = "assets/customModifiersInMelvor/Generic_Monster_Type_Icon.png";
 
-    static readonly ANIMAL_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/monsters/frozen_mammoth.png";
-    static readonly DEMON_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/monsters/fierce_devil.png";
-    static readonly DRAGON_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/monsters/dragon_green.png";
-    static readonly ELEMENTAL_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/monsters/fire_spirit.png";
-    static readonly HUMAN_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/skills/thieving/man.svg";
-    static readonly MYTHICAL_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/monsters/griffin.png";
-    static readonly SEA_CREATURE_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/monsters/large_crab.png";
-    static readonly UNDEAD_MODIFIER_ICON_RESOURCE_URL = "https://cdn.melvor.net/core/v018/assets/media/monsters/skeleton.png";
+    // (Non-mod) Images can just be referenced using their path (which preferably checks the game directory and falls back to cdn)
+    static readonly ANIMAL_MODIFIER_ICON_RESOURCE_URL = "assets/media/monsters/frozen_mammoth.png";
+    static readonly DEMON_MODIFIER_ICON_RESOURCE_URL = "assets/media/monsters/fierce_devil.png";
+    static readonly DRAGON_MODIFIER_ICON_RESOURCE_URL = "assets/media/monsters/dragon_green.png";
+    static readonly ELEMENTAL_MODIFIER_ICON_RESOURCE_URL = "assets/media/monsters/fire_spirit.png";
+    static readonly HUMAN_MODIFIER_ICON_RESOURCE_URL = "assets/media/skills/thieving/man.svg";
+    static readonly MYTHICAL_MODIFIER_ICON_RESOURCE_URL = "assets/media/monsters/griffin.png";
+    static readonly SEA_CREATURE_MODIFIER_ICON_RESOURCE_URL = "assets/media/monsters/large_crab.png";
+    static readonly UNDEAD_MODIFIER_ICON_RESOURCE_URL = "assets/media/monsters/skeleton.png";
 }
