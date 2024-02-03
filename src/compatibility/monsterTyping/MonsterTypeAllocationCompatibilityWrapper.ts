df53e2ea08eea89d730343aa47006fcd082154c2
KumaV1 <KumaV1>
2024-02-03 13:40:06 +0100

    Added monster type allocation for "Godwars Recreation" mod

--- src/compatibility/monsterTyping/MonsterTypeAllocationCompatibilityWrapper.ts
+++ src/compatibility/monsterTyping/MonsterTypeAllocationCompatibilityWrapper.ts
@@ -1,13 +1,17 @@
 import { AbyssalRiftMtaCompatibility } from "./AbyssalRiftMtaCompatibility";
+import { GwdMtaCompatibility } from "./GwdMtaCompatibility";
 import { MusicMtaCompatibility } from "./MusicMtaCompatibility";
 
 /** A little wrapper of all those monster type allocation compatibility */
 export class MonsterTypeAllocationCompatibilityWrapper {
     public static registerMonsterTypeAllocations(ctx: Modding.ModContext) {
-        const mythMusicCompatibility = new MusicMtaCompatibility(ctx);
-        mythMusicCompatibility.allocateMonsterTypes();
+        const mythMusic = new MusicMtaCompatibility(ctx);
+        mythMusic.allocateMonsterTypes();
 
-        const abyRiftCompatibility = new AbyssalRiftMtaCompatibility(ctx);
-        abyRiftCompatibility.allocateMonsterTypes();
+        const abyRift = new AbyssalRiftMtaCompatibility(ctx);
+        abyRift.allocateMonsterTypes();
+
+        const gwd = new GwdMtaCompatibility(ctx);
+        gwd.allocateMonsterTypes();
     }
 }
